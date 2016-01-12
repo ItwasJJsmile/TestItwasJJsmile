d82bef48eb855ebb0d0db45c02b67831cbd0d6c1
ItwasJJsmile <itwasjjsmile@gmail.com>
2016-01-12 12:00:55 +0800

    commit

--- src/com/javabase/test/IfEqual.java
+++ src/com/javabase/test/IfEqual.java
@@ -1,10 +1,24 @@
 package com.javabase.test;
 
+import java.util.HashSet;
+
 public class IfEqual {
 	public static void main(String[] args) {
-		String a = "test";
-		String b = "test";
-		System.out.println(a.equals(b));
-		System.out.println(a==b);
+//		Emp emp1 = new Emp(23);
+//		Emp emp2 = new Emp(24);
+//		Emp emp3 = new Emp(25);
+//		Emp emp4 = new Emp(26);
+//		Emp emp5 = new Emp(27);
+//		HashSet<Emp> hs = new HashSet<Emp>();
+//		hs.add(emp1);
+//		hs.add(emp2);
+//		hs.add(emp3);
+//		hs.add(emp4);
+//		hs.add(emp5);
+		
+//		System.out.println("HashSet Size--->>>"+hs.size());
+//		System.out.println("hs.contains( new Emp(25))--->>>"+hs.contains(new Emp(25)));
+//		System.out.println("hs.remove( new Emp(24)--->>>"+ hs.remove( new Emp(24)));
+//		System.out.println("Now HashSet Size--->>>"+hs.size());
 	}
 }
